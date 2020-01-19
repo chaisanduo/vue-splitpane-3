f758ac077e9e15baee08191ed599a2e719153842
chaisanduo <33170352+chaisanduo@users.noreply.github.com>
2020-01-19 11:35:28 +0800

    Add files via upload
    上传拖拽组件

---
+++ draggable.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <draggable :list="list" v-bind="$attrs" @end="end">
+      <div v-for="element in list" :key="element.id">
+        <div>
+          <component :is="element.componentname" />
+        </div>
+      </div>
+    </draggable>
+  </div>
+</template>
+<script>
+import draggable from 'vuedraggable'
+
+// 内容组件
+import JnPopuplefttop from './components/JnPopuplefttop'
+import JnPopupcentertop from './components/JnPopupcentertop'
+import JnPopupcenterbottom from './components/JnPopupcenterbottom'
+import JnPopupleftbottom from './components/JnPopupleftbottom'
+import JnPopuprighttop from './components/JnPopuprighttop'
+import JnPopuprightbottom from './components/JnPopuprightbottom'
+export default {
+  name: 'DragDemo',
+  components: {
+    draggable,
+    JnPopuplefttop,
+    JnPopupcentertop,
+    JnPopupleftbottom,
+    JnPopupcenterbottom,
+    JnPopuprighttop,
+    JnPopuprightbottom
+  },
+  props: {
+    list: {
+      type: Array,
+      default() {
+        return [
+          JnPopuplefttop,
+          JnPopupcentertop,
+          JnPopupleftbottom,
+          JnPopupcenterbottom,
+          JnPopuprighttop,
+          JnPopuprightbottom
+        ]
+      }
+    }
+  },
+  methods: {
+    end: function() {
+
+    }
+  }
+}
+</script>
+<style lang="scss">
+
+</style>
